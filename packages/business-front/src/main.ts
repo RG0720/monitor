9ca923d4bb7e49aa2ed0c00e0d353a98f043bf16
annie1015 <371046515@qq.com>
2021-11-14 20:18:11 +0800

    feat: 增加arco-design、vue-router

--- packages/business-front/src/main.ts
+++ packages/business-front/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './router/index'
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+const app = createApp(App);
+app.use(router)
+app.use(ArcoVue);
+app.mount('#app');
